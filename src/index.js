eab05ecb316b2ee30fa550322166c61298e935fe
Sjon de Gast <sjondegast@gmail.com>
2019-10-26 14:10:50 +0200

    added prettier config and install local devDependancie

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const title = 'react-webpack-boilerplate';
 
 ReactDOM.render(
   <App title={title} />,
-  document.getElementById('app')
+  document.getElementById('app'),
 );
 
 module.hot.accept();
