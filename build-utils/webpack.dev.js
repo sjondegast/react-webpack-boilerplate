cbf3ffb804441ffba7a744248c3e75871d11ad9e
Sjon de Gast <sjondegast@gmail.com>
2019-11-11 21:53:04 +0100

    added packages for loading sass and extract css from bundle.js into separate files... need to setup/config the loaders and extract the css plus adding content hashes in development

--- build-utils/webpack.dev.js
+++ build-utils/webpack.dev.js
@@ -1,6 +1,8 @@
 const webpack = require('webpack');
 const Dotenv = require('dotenv-webpack');
 
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = {
   mode: 'development',
   devtool: 'eval-source-map',
