88db3e41f58b2533905fb8a11e3b96c609de54be
Sjon de Gast <sjondegast@gmail.com>
2019-12-01 15:58:06 +0100

    setup code chunks all and also node_modules in their own chunks files to reduce file size

--- build-utils/webpack.prod.js
+++ build-utils/webpack.prod.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const Dotenv = require('dotenv-webpack');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
@@ -26,6 +27,27 @@ module.exports = {
         },
       }),
     ],
+    runtimeChunk: 'single',
+    splitChunks: {
+      chunks: 'all',
+      maxInitialRequests: Infinity,
+      minSize: 0,
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name(module) {
+            // get the name. E.g. node_modules/packageName/not/this/part.js
+            // or node_modules/packageName
+            const packageName = module.context.match(
+              /[\\/]node_modules[\\/](.*?)([\\/]|$)/,
+            )[1];
+
+            // npm package names are URL-safe, but some servers don't like @ symbols
+            return `npm.${packageName.replace('@', '')}`;
+          },
+        },
+      },
+    },
   },
   devtool: 'source-map',
   plugins: [
@@ -36,6 +58,7 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: '[name].[contentHash].css',
     }),
+    new webpack.HashedModuleIdsPlugin(), // so that file hashes don't change unexpectedly
   ],
   module: {
     rules: [
