4e1a55221c8ff3c977d24f514c10b91813097e2a
Sjon de Gast <sjondegast@gmail.com>
2019-12-01 14:47:16 +0100

    added vendorjs file for importing js libraries

--- src/vendor.js
+++ src/vendor.js
@@ -1,2 +1,9 @@
-// eslint-disable-next-line no-console
-console.log('hi from Boilerplate');
+/* 
+********** Vendor.js ****************
+import js libraries like bootstrap and others.
+the imports in vendor js are less likely to change and are compiled into their own file.
+
+example:
+// first install npm install --save bootstrap
+import "bootstrap";
+*/
