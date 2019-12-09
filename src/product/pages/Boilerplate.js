9273a811540729bf67ae1467367f068329757ddf
Sjon de Gast <sjondegast@gmail.com>
2019-12-09 17:05:15 +0100

    restructured src folder, added normalize.scss and restructured folder structure for atomic design

---
+++ src/product/pages/Boilerplate.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Title from '../../system/Atoms/title';
+
+const App = () => {
+  return (
+    <div>
+      <Title title=" Webpack, Babel, React Boilerplate" />
+    </div>
+  );
+};
+
+export default App;
